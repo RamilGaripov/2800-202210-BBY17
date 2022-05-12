d4151b1f546f3b2d1c3dd472f1edee8b83939768
Ramil Garipov <garipovramil93@hotmail.ca>
2022-05-12 16:23:51 -0700

    Added a confirmation pop-up for the "Delete User" function on the admin dashboard. Also, the table of users gets updated without refreshing the page.

--- public/js/admin.js
+++ public/js/admin.js
@@ -4,11 +4,11 @@ async function getAccounts() {
         let response = await fetch("/get-accounts", {
             method: "GET"
         });
-        
+
         //ATTRIBUTION: 
         //This table creation code is taken from the examples of our COMP2537 professor, Mr. Arron Ferguson. 
         //RDBMS-CRUD MySQL example. 
-        
+
         if (response.status === 200) {
             const data = await response.json();
             // console.log("the data: ", data);
@@ -20,74 +20,76 @@ async function getAccounts() {
                 <th class="edit_header"></th>
                 <th class="delete_header"></th>
                 </tr>`;
-                
-                    for(let i = 0; i < data.rows.length; i++) {
-                        let row = data.rows[i];
-                        //console.log("row", row);
-                        str += ("<tr><td class='id'>" + row.id
-                            + "</td><td class='first_name'>" + row.first_name
-                            + "</td><td class='last_name'>" + row.last_name
-                            + "</td><td class='email'>" + row.email
-                            + "</td><td class='edit'>" + "<button type='submit' class='edit_user'>Edit</button>"
-                            + "</td><td class='delete'>" + "<button type='submit' class='delete_user'>Delete</button>"
-                            + "</td></tr>");
-                        // id_array.push(row)
-                    }
 
-                    //provides EDIT BUTTON functionality 
-                    document.getElementById("accounts").innerHTML = str;
-                    const edits = document.getElementsByClassName("edit_user");
-                    for (let j = 0; j < edits.length; j++) {
-                        edits[j].addEventListener("click", function(e) {
-                            e.preventDefault();
-                            console.log("Let's edit the user with id", data.rows[j].id);
-                            //When calling this function, we're passing a JSON object in the parameters, using {key : value} format.
-                            editUser({
-                                id: data.rows[j].id
-                            });
-                        })
-                    }
+            for (let i = 0; i < data.rows.length; i++) {
+                let row = data.rows[i];
+                //console.log("row", row);
+                str += ("<tr><td class='id'>" + row.id +
+                    "</td><td class='first_name'>" + row.first_name +
+                    "</td><td class='last_name'>" + row.last_name +
+                    "</td><td class='email'>" + row.email +
+                    "</td><td class='edit'>" + "<button type='submit' class='edit_user'>Edit</button>" +
+                    "</td><td class='delete'>" + "<button type='submit' class='delete_user'>Delete</button>" +
+                    "</td></tr>");
+                // id_array.push(row)
+            }
 
-                    //provides DELETE BUTTON functionality 
-                    const deletes = document.getElementsByClassName("delete_user");
-                    for (let k = 0; k < deletes.length; k++) {
-                        deletes[k].addEventListener("click", function(e) {
-                            e.preventDefault();
-                            console.log("Let's delete the user with id", data.rows[k].id);
-                            deleteUser({
-                                id: data.rows[k].id
-                            });
+            //provides EDIT BUTTON functionality 
+            document.getElementById("accounts").innerHTML = str;
+            const edits = document.getElementsByClassName("edit_user");
+            for (let j = 0; j < edits.length; j++) {
+                edits[j].addEventListener("click", function (e) {
+                    e.preventDefault();
+                    console.log("Let's edit the user with id", data.rows[j].id);
+                    //When calling this function, we're passing a JSON object in the parameters, using {key : value} format.
+                    editUser({
+                        id: data.rows[j].id
+                    });
+                })
+            }
+
+            //provides DELETE BUTTON functionality 
+            const deletes = document.getElementsByClassName("delete_user");
+            for (let k = 0; k < deletes.length; k++) {
+                deletes[k].addEventListener("click", function (e) {
+                    e.preventDefault();
+                    if (confirm("Delete " + data.rows[k].first_name + " " + data.rows[k].last_name + "'s account?")) {
+                        console.log("Let's delete the user with id", data.rows[k].id);
+                        deleteUser({
+                            id: data.rows[k].id
                         });
                     }
+                });
+            }
         } else {
             console.log(response.status);
         }
 
-    } catch(error) {
+    } catch (error) {
         console.log(error);
     }
 }
 
 //sends id of the user to the server. Redirects to the edit.html
 async function editUser(data) {
-    try{
-    // console.log("Edit user activated.");
-    // console.log(data);
-    const response = await fetch("/edit-user", {
-        method: "POST",
-        headers: {
-            "Accept" : "application/json",
-            "Content-Type" : "application/json"
-          },
-        body: JSON.stringify(data)
-    });
-    let parsedJSON = await response.json();    
-    if (parsedJSON.status == "fail") {
-        console.log("Couldn't edit this user's info.");
-      } else {
-        window.location.replace("/edit");
-      }
-    } catch(err) {
+    try {
+        // console.log("Edit user activated.");
+        // console.log(data);
+        const response = await fetch("/edit-user", {
+            method: "POST",
+            headers: {
+                "Accept": "application/json",
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(data)
+        });
+        let parsedJSON = await response.json();
+        if (parsedJSON.status == "fail") {
+            console.log("Couldn't edit this user's info.");
+        } else {
+            window.location.replace("/edit");
+        }
+    } catch (err) {
         console.log(err);
     }
 }
@@ -97,22 +99,24 @@ async function deleteUser(data) {
     try {
         // console.log("Delete user activated");
         // console.log(data);
+
         const response = await fetch("/delete-user", {
             method: "POST",
             headers: {
-                "Accept" : "application/json",
-                "Content-Type" : "application/json"
-              },
+                "Accept": "application/json",
+                "Content-Type": "application/json"
+            },
             body: JSON.stringify(data)
         });
-        let parsedJSON = await response.json();    
+        let parsedJSON = await response.json();
         if (parsedJSON.status == "fail") {
             console.log("Cannot delete this user.");
-          } else {
+        } else {
             console.log("User deleted.")
-            window.location.replace("/dashboard");
-          }
-    } catch(err) {
+            getAccounts();
+        }
+
+    } catch (err) {
         console.log(err);
     }
 }
