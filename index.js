28e406ec5b8ae2045a64d6b8980a9ef158059b04
Joshua Chen <joshuachenyyc@gmail.com>
2022-05-18 14:54:49 -0700

    removed port

--- index.js
+++ index.js
@@ -719,9 +719,3 @@ http.createServer((req, res) => {
 
 
 
-app.set('port', (process.env.PORT || 5000));
-
-
-app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
-});
