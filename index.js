79bfd89de769abc019581260adb9161b04527d0e
Joshua Chen <joshuachenyyc@gmail.com>
2022-05-21 13:20:34 -0700

    changed port

--- index.js
+++ index.js
@@ -723,7 +723,7 @@ async function init() {
 // Sets the port and runs the server. Calls init().
 let port = process.env.PORT || 8000;
 
-app.listen(80, function() {
+app.listen(port, function() {
   init();
 });
 
