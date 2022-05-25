0ec3b07a87ffa52a4f4d2e1fb05f93ac1a117651
Ramil Garipov <garipovramil93@hotmail.ca>
2022-05-25 12:43:40 -0700

    Added pop up warnings for unimplemented features.

---
+++ public/js/reward.js
@@ -0,0 +1,29 @@
+document.getElementById("redeemGold").addEventListener("click", function(e) {
+    e.preventDefault();
+    console.log("NOT IMPLEMENTED");
+    Swal.fire(
+        'Sorry!',
+        "This feature is coming soon.",
+        'info'
+    )
+}) 
+
+document.getElementById("redeemSilver").addEventListener("click", function(e) {
+    e.preventDefault();
+    console.log("NOT IMPLEMENTED");
+    Swal.fire(
+        'Sorry!',
+        "This feature is coming soon.",
+        'info'
+    )
+}) 
+
+document.getElementById("redeemBronze").addEventListener("click", function(e) {
+    e.preventDefault();
+    console.log("NOT IMPLEMENTED");
+    Swal.fire(
+        'Sorry!',
+        "This feature is coming soon.",
+        'info'
+    )
+}) 
