519590d535991a69bb5cdb10f7f24f0d6656b395
Ramil Garipov <garipovramil93@hotmail.ca>
2022-05-26 09:02:18 -0700

    Some minor bug fixes

--- public/js/reward.js
+++ public/js/reward.js
@@ -1,29 +1,26 @@
 document.getElementById("redeemGold").addEventListener("click", function(e) {
     e.preventDefault();
-    console.log("NOT IMPLEMENTED");
     Swal.fire(
         'Sorry!',
-        "This feature is coming soon.",
+        "Hold on to your points! This feature is coming soon.",
         'info'
     );
 }) ;
 
 document.getElementById("redeemSilver").addEventListener("click", function(e) {
     e.preventDefault();
-    console.log("NOT IMPLEMENTED");
     Swal.fire(
         'Sorry!',
-        "This feature is coming soon.",
+        "Hold on to your points! This feature is coming soon.",
         'info'
     );
 }) ;
 
 document.getElementById("redeemBronze").addEventListener("click", function(e) {
     e.preventDefault();
-    console.log("NOT IMPLEMENTED");
     Swal.fire(
         'Sorry!',
-        "This feature is coming soon.",
+        "Hold on to your points! This feature is coming soon.",
         'info'
     );
 }) ;
